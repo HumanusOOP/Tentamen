8c490c858003dbd1df4263301ffb98a38437e194
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2018-11-21 14:06:13 +0100

    Update Övningar.cs

--- Övningar.cs
+++ Övningar.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 
 public interface Övningar {
+    // Summera alla tal i listan. Använd inte .Sum() ;)
     int Summa(List<int> heltal);
+    
+    // Om heltal är jämnt delbart med 3, returnera "Fizz"
+    // Om heltal är jämnt delbart med 5, returnera "Buzz"
+    // Om heltal är jämnt delbart med 3 och 5, returnera "FizzBuzz"
     string FizzBuzz(int heltal);
 
+    // Lös fibonacci rekursivt
     int FibonacciRec(int n); // Lös rekursivt
 
-    int Fibonacci(int n); // Lös O(n)
+    // Lös fibonacci utan rekursion (O(n))
+    int Fibonacci(int n);
 
+    // Lös fibonacci utan rekursion O(n) och med konstant minnesanvändning
+    int Fibonacci2(int n);
+    
     // Returns the position of `elementToFind` in array
     // Assume that array is ordered/sorted ascending and 
     // that an element can only appear once in the array
